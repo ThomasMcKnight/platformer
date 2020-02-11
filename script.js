b2da557741504ec9dcb8ff54baff7e7e6edeea00
ThomasMcKnight <thomasmcknight711@gmail.com>
2020-02-10 21:44:19 -0500

    Added player and platform objects

---
+++ script.js
@@ -0,0 +1,46 @@
+class Platform 
+{
+   constructor()
+   {
+       this.height = 10;
+       this.width = 100;
+       this.position = 
+       {
+           x: 500,
+           y: 300
+       };
+   }
+   draw(ctx)
+   {
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+   }
+}
+
+class Player
+{
+    constructor()
+    {
+        this.height = 30;
+        this.width = 30;
+        this.position = 
+        {
+            x: 300,
+            y: GAMEHEIGHT - this.height
+        };
+    }
+    draw(ctx)
+    {
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+const GAMEHEIGHT = 480;
+const GAMEWIDTH = 720;
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
+let platform = new Platform();
+let player = new Player();
+platform.draw(ctx);
+player.draw(ctx);
+
+
